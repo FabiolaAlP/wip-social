533fc34360c33d10f050cd968ea93108e61b7e37
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-12-03 15:51:11 -0600

    add tanstack query provider, client and devtools setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,24 @@ import App from './App.jsx'
 import { ThemeProvider } from './Context/ThemeContext.jsx'
 import { AuthProvider } from './Context/AuthContext.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import AppRoutes from './Routes/AppRoutes.jsx'
+const queryClient = new QueryClient();
 const router = createBrowserRouter(AppRoutes);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <RouterProvider router={router}>
-          <App />
-        </RouterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}>
+            <App />
+          </RouterProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
