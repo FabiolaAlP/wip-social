88bea17b0022e869a8d279d8cac567738b9ff4cc
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-12-16 16:49:23 -0600

    style: apply flex styles to user profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -13,11 +13,14 @@ const Profile = () => {
     }
     return (
         <div className='w-4/5 max-w-4xl p-6 mx-auto border-2 rounded-lg shadow-lg bg-white/50 dark:bg-slate-700/30 backdrop-blur-lg border-slate-300 dark:border-slate-200'>
-            <h3 className='text-2xl text-black text-balance dark:text-white'>{user.username}</h3>
-            <span className='text-sm font-bold text-slate-500 dark:text-slate-200'>Email: {user.email}</span>
-            {user.avatar && (
-                <img src={user.avatar} alt='user avatar' className='mt-4 rounded-full' />
-            )}
+            <section className='flex flex-col items-center justify-center gap-2'>
+                {user.avatar && (
+                    <img src={user.avatar} alt='user avatar' className='mt-4 rounded-full' />
+                )}
+                <h3 className='text-2xl text-black text-balance dark:text-white'>{user.username}</h3>
+                <span className='text-sm font-bold text-slate-500 dark:text-slate-200'>Email: {user.email}</span>
+            </section>
+
         </div>
     )
 }
